b7d37c005f486b6d0d4684d73bf31404f4523f92
Aakriti Arya <aakritiarya2005@gmail.com>
2026-04-11 16:54:00 +0530

    User Input Ways and Program

--- JavaScript/userInput.js
+++ JavaScript/userInput.js
@@ -5,10 +5,12 @@
 
 let username = window.prompt("What's your username?")
 console.log(username);
+document.getElementById("myP5").textContent = `username entered is ${username}`
 
 //Professional Way -> HTML Textbox
 let StudentId;
 document.getElementById("mySubmit").onclick = function(){
     StudentId = document.getElementById("myText").value;
     console.log(StudentId);
+    document.getElementById("myP6").textContent = `StudentId entered is ${StudentId}`
 }
