5cea209986328fbd8a9cd6e923ecc24b9d680d83
Aakriti Arya <aakritiarya2005@gmail.com>
2026-04-11 13:21:47 +0530

    intro

---
+++ JavaScript/variables.js
@@ -0,0 +1,7 @@
+let fullName = "Aakriti Arya"
+let rollNumber = 59
+let pass = true
+
+document.getElementById("myH1").textContent = fullName;
+document.getElementById("myP1").textContent = rollNumber;
+document.getElementById("myP2").textContent = pass;
